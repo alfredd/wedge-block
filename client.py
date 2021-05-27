4e163e0b30add25df2e5d1647e1f0729312975f3
YinanDanielZhou <zhouyinan49@gmail.com>
2021-05-27 11:18:19 -0700

    modified Hash1 to include the merkle root and proof. Client can now verify the correctness of hash1.

--- client.py
+++ client.py
@@ -1,8 +1,15 @@
 import grpc
 import wedgeblock_pb2_grpc as wbgrpc
 import wedgeblock_pb2 as wb
-import sys
 
+import sys
+import merklelib
+import hashlib
+import pickle
+
+
+def hashfunc(value):
+    return hashlib.sha256(value).hexdigest()
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
@@ -14,6 +21,15 @@ def run():
         t = wb.Transaction(rw=wb.RWSet(type=wb.TxnType.RW, key=sys.argv[1], val=sys.argv[2]))
         print("Sending t: %s" %t)
         hash1 = stub.Execute(t)
+
+        merkle_proof = pickle.loads(hash1.merkleProof) # deserialize
+        data = (hash1.rw.key, hash1.rw.val)
+
+        if merklelib.verify_leaf_inclusion(data, merkle_proof, hashfunc, hash1.merkleRoot):
+            print("hash1 is correct")
+        else:
+            print("something wrong")
+
         print("Received hash1: %s" %hash1)
 
         hash2 = stub.GetPhase2Hash(wb.LogHash(logIndex=hash1.logIndex))
